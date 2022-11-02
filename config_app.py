c69e9a069e907d335aca14fe32374380357e9219
Sayli Tawhare <sayli.tawhare@gmail.com>
2022-11-02 11:31:44 +0530

    small format change

--- config_app.py
+++ config_app.py
@@ -5,19 +5,27 @@ def func1(config_file):
     data = json.load(file)
 
     config_text = open("config_nvdsanalytics.txt","w")
+    config_text.close()
+    config_text = open("config_nvdsanalytics.txt", "a")
 
     for key, value in data.items():
+        
         config_text.write('[{key}]\n'.format(key=key))
-    
-        config_text = open("config_nvdsanalytics.txt", "a")
 
         for key,value in value.items():
-            config_text.write(
+            if type(value) is list:
+                list_value = []
+                for element in value:
+                    list_value.append(str(element))
+                    list_type_value = ";".join(list_value)
+                config_text.write(
+                    '{key}={list_type_value}\n'.format(key=key,list_type_value=list_type_value))
+            else:
+                config_text.write(
                 '{key}={value}\n'.format(key=key,value=value))
             
         config_text.write("\n")
 
-    config_text.close()
     config_text.close()
 
     
